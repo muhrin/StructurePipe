1f259dc8ca3acfbc7390ebd0e512ae7be2799df7
Martin Uhrin <martin.uhrin@gmail.com>
2012-10-20 13:14:44 +0100

    Some more work on structure comparators test

---
+++ lib/sslib/tests/utility/StructureComparatorsTest.cpp
@@ -0,0 +1,35 @@
+/*
+ * DistanceCalculatorsTest.cpp
+ *
+ *  Created on: Aug 18, 2011
+ *      Author: Martin Uhrin
+ */
+
+// INCLUDES //////////////////////////////////
+#include "sslibtest.h"
+
+#include <boost/filesystem.hpp>
+
+#include <common/Structure.h>
+#include <io/ResReaderWriter.h>
+#include <utility/DistaceMatrixComparator.h>
+#include <utility/SortedDistanceComparator.h>
+#include <utility/SortedDistanceComparatorEx.h>
+
+namespace fs = ::boost::filesystem;
+namespace ssbc = ::sstbx::build_cell;
+namespace ssc = ::sstbx::common;
+namespace ssu = ::sstbx::utility;
+
+
+BOOST_AUTO_TEST_CASE(StructureComparatorsTest)
+{
+  // SETTINGS ////////////////
+  const fs::path referenceStructuresPath("similarStructures");
+
+  BOOST_REQUIRE(fs::exists(referenceStructuresPath));
+
+  BOOST_REQUIRE(fs::is_directory(referenceStructuresPath));
+
+
+}
